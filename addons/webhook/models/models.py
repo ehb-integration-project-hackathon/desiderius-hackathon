100e55f1f1ad3e32e2c4b4d4e36dc636bed57413
doganhasko <doganhasko@gmail.com>
2024-04-26 18:01:13 +0200

    Adapted Uuid

--- addons/webhook/models/models.py
+++ addons/webhook/models/models.py
@@ -1,6 +1,5 @@
 from odoo import models, fields, api
 import pika
-import json
 import datetime
 import uuid
 
@@ -9,13 +8,13 @@ class WebhookWebhook(models.Model):
     _description = 'Webhook Webhook'
 
     name = fields.Char('name', required=True)
-    uid = fields.Char('uid')  # Define custom_id as a Char field to store the custom ID
+    Uuid = fields.Char('Uuid')  # Define custom_id as a Char field to store the custom ID
 
 
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
-    uid = fields.Char('uid', unique=True)  # Define custom_id as a Char field to store the custom ID
+    Uuid = fields.Char('Uuid', unique=True)  # Define custom_id as a Char field to store the custom ID
     @api.model
     def create(self, vals):
         print("UPdate called 1")
@@ -24,13 +23,13 @@ class ResPartner(models.Model):
         if context.get('bypass_webhook', False):
             new_partner = super(ResPartner, self).create(vals)
         else:
-            # Generate a new UUID if uid is not provided
-            if 'uid' not in vals or not vals['uid']:
-                vals['uid'] = str(uuid.uuid4())  # Generate a new UUID
+            # Generate a new UUuid if Uuid is not provided
+            # if 'Uuid' not in vals or not vals['Uuid']:
+            vals['Uuid'] = str(uuid.uuid4())  # Generate a new UUuid
             print("UPdate called 2")
 
             new_partner = super(ResPartner, self).create(vals)
-            self.send_user_webhook(new_partner, 'Create')
+            self.send_user_webhook(vals['Uuid'], 'Create')
 
         return new_partner
 
@@ -43,40 +42,62 @@ class ResPartner(models.Model):
 
     @api.model
     def write(self, vals):
-        print("UPdate called")
-        if 'uid' in vals:
-            uid = vals.pop('uid')
-            partners_to_update = self.env['res.partner'].sudo().search([('uid', '=', uid)])
-            if partners_to_update:
-                res = partners_to_update.write(vals)
-                if res:
-                    self.send_user_webhook(uid, 'Create')
-                    self.send_user_webhook(uid, 'Update')
+        # print("UPdate called")
+        # self.send_user_webhook(Uuid, 'Update')  # Only send the 'Update' webhook
+        #
+        # Uuid = vals.get('Uuid', False)
+        # if Uuid:
+        #     print("Uuid=" + Uuid)
+        #
+        # # Call the super method to perform the actual write operation
+        # return super(ResPartner, self).write(vals)
 
-                return res
-            else:
-                return False
-        else:
-            res = super(ResPartner, self).write(vals)
-            self.send_user_webhook(self.uid, 'Create')
-            self.send_user_webhook(self.uid, 'Update')
+        res = super(ResPartner, self).write(vals)
+        for partner in self:
+            print("UPdate called4")
+            self.send_user_webhook(partner.Uuid, 'Update')  # Only send the 'Update' webhook
 
-            return res
+        return res
+        # if 'Uuid' in vals:
+        #     Uuid = vals.pop('Uuid')
+        #     partners_to_update = self.env['res.partner'].sudo().search([('Uuid', '=', Uuid)])
+        #     if partners_to_update:
+        #         res = partners_to_update.write(vals)
+        #         if vals['Uuid']:
+        #             print("UPdate called3")
+        #
+        #             # self.send_user_webhook(Uuid, 'Update')  # Only send the 'Update' webhook
+        #         return res
+        #     else:
+        #         return False
+        # else:
+        #     res = super(ResPartner, self).write(vals)
+        #     for partner in self:
+        #         print("UPdate called4")
 
-    def unlink(self,uid):
-        self.send_user_webhook(self, 'Delete')
+
+        # self.send_user_webhook(partner.Uuid, 'Update')  # Only send the 'Update' webhook
+
+        #return res
+
+    # def unlink(self,Uuid):
+    #     self.send_user_webhook(self, 'Delete')
+    #     return super(ResPartner, self).unlink(Uuid)
+    #
+    def unlink(self):
+        for partner in self:
+            self.send_user_webhook(partner.Uuid, 'Delete')
         return super(ResPartner, self).unlink()
 
-
-    def send_user_webhook(self, uid, crud_tag):
+    def send_user_webhook(self, Uuid, crud_tag):
         # Connect to RabbitMQ server
         connection = pika.BlockingConnection(pika.ConnectionParameters('rabbitmq'))
         channel = connection.channel()
 
-        # Get the partner record using the uid
-        partner = self.env['res.partner'].sudo().search([('uid', '=', str(uid))], limit=1)
+        # Get the partner record using the Uuid
+        partner = self.env['res.partner'].sudo().search([('Uuid', '=', str(Uuid))], limit=1)
 
-        # Check if a partner with the given uid exists
+        # Check if a partner with the given Uuid exists
         if partner:
             # Create XML message
             xml_message = self.create_xml_message(partner, crud_tag)
@@ -88,7 +109,7 @@ class ResPartner(models.Model):
             channel.basic_publish(exchange='', routing_key='sendgrid-queue', body=xml_message)
             channel.basic_publish(exchange='', routing_key='wordpress-queue', body=xml_message)
         else:
-            # Handle the case when no partner is found with the given uid
+            # Handle the case when no partner is found with the given Uuid
             # You can log an error or take appropriate action here
             pass
 
@@ -99,6 +120,7 @@ class ResPartner(models.Model):
         # Construct XML message according to the schema
         user_xml = '<?xml version="1.0" encoding="UTF-8"?>\n'
         user_xml += '<User xmlns="http://www.example.com/user">\n'
+        user_xml += ' <Uuid>{}</Uuid>\n'.format(partner.Uuid or "")
         user_xml += ' <FirstName>{}</FirstName>\n'.format(partner.name)
         user_xml += ' <LastName>Unknown</LastName>\n'
         user_xml += ' <BirthDate>2000-01-01</BirthDate>\n'
@@ -112,6 +134,5 @@ class ResPartner(models.Model):
         user_xml += ' </Address>\n'
         user_xml += ' <Timestamp>{}</Timestamp>\n'.format(datetime.datetime.now().isoformat())
         user_xml += ' <CRUD>{}</CRUD>\n'.format(crud_tag)
-        user_xml += ' <Uid>{}</Uid>\n'.format(partner.uid or "")
         user_xml += '</User>\n'
         return user_xml
